06849f157658fd8ee48a42e707f4683a7f7d712d
Gabriel Rangel <glrangel25@gmail.com>
2017-03-15 15:34:46 -0700

    Formatted fav/unfav button for 3 presidents, ready to implement to rest

--- public/assignment5/framework/hw5/wireframes/favorites.js
+++ public/assignment5/framework/hw5/wireframes/favorites.js
@@ -5,48 +5,63 @@ var config = {
     storageBucket: "cse-134b-b2127.appspot.com",
     messagingSenderId: "298947904638"
 };
-var listOfFavs = [];
 var app = firebase.initializeApp(config);
+var finalSubmissionURL = "https://cse-134b-b2127.firebaseapp.com/assignment4/framework/hw4/wireframes/favorites.html";
+var debugURL = "file:///Users/TheShire/Google%20Drive/UCSD/Winter%202017/CSE%20134B/cse134/public/assignment5/framework/hw5/wireframes/favorites.html";
+//change debugURL to your URL that is displayed when you click on the my favorites tab
+
 
 firebase.auth().onAuthStateChanged(firebaseUser => {
     if(firebaseUser) {
-    	console.log(window.location);
-    	if(window.location == "https://cse-134b-b2127.firebaseapp.com/assignment4/framework/hw4/wireframes/favorites.html")
-      		myFavs(firebaseUser.uid); //dont call if on presidents
+    // 	console.log(window.location);
+    if(window.location == debugURL) //only search users database if on my favorites page
+      	myFavs(firebaseUser.uid); //populates users favorites
+    else
+    	isFav(firebaseUser.uid);
       console.log(firebaseUser.email + " is logged in!" );
     } else {
       console.log('not logged in');
     }
 });
-// var listoffavorites = [];
 
 function addFavoritePresident(userId, president) {
 	//reveal unfav button
 	document.getElementById('favBtn').style.zIndex = 0;
-	alert(president.presidentname + " was favorited.");
+	// alert(president.presidentname + " was favorited.");
     var favListRef = firebase.database().ref('users/' + userId + '/listoffavorites');
     var newFav = favListRef.push();
     newFav.set(president);
+    listOfFavs.push(president);
 }
 function removeFavoritePresident(userId, president) {
-	
+	var currentPresName = president.presidentname; //taken from specifc_president.html script (i.e. johnadams.html has a script with his name)
 	document.getElementById('favBtn').style.zIndex = 2;
-	alert("unfav: " + president.presidentname);
-	// alert(president.presidentname + " was favorited.");
- //    var favListRef = firebase.database().ref('users/' + userId + '/listoffavorites');
- //    var newFav = favListRef.push();
- //    newFav.set(president);
+	var ref = firebase.database().ref('users/' + userId+ '/listoffavorites');
+	
+
+	ref.once('value',function(snapshot){
+			snapshot.forEach(function(childSnapshot) { //check each fav president if match change fav button
+		    var childKey = childSnapshot.key;
+		    var childData = childSnapshot.val();
+		    // console.log(childKey);
+		    if(childData.presidentname == currentPresName){
+		    	//show unfav button ontop
+		    	ref.child(childKey).remove();
+		    	document.getElementById('favBtn').style.zIndex = 2;
+		    	
+		    	// alert("unfav: " + president.presidentname);
+		    }
+	  	});
+	});
 }
-
-
 function myFavs(userId){
 	var ref = firebase.database().ref('users/' + userId+ '/listoffavorites');
 	
 	ref.on("child_added", function(data, prevChildKey) {
 		var newFavPres = data.val();
-		console.log("profile_link: " + newFavPres.profile_link);
-		console.log("image_source: " + newFavPres.image_source);
-		console.log("presidentname: " + newFavPres.presidentname);
+		// console.log("profile_link: " + newFavPres.profile_link);
+		// console.log("image_source: " + newFavPres.image_source);
+		// console.log("presidentname: " + newFavPres.presidentname);
 		
 		var div = document.createElement('div');
 		div.className += ('pure-u-1 pure-u-md-1-3');
@@ -54,17 +69,24 @@ function myFavs(userId){
         '<img src="pres_images/'+  newFavPres.image_source +'" ' +  'alt="' + newFavPres.presidentname + '">'+ '</a><p>' + newFavPres.presidentname+ '</p></section>';
 
 		document.getElementById('list').appendChild(div);
-
-
 	});
 }
 
-
-
-
-
-
-
+function isFav(userId){
+	var currentPresName = president.presidentname; //taken from specifc_president.html script (i.e. johnadams.html has a script with his name)
+	var ref = firebase.database().ref('users/' + userId+ '/listoffavorites');
+	ref.once('value',function(snapshot){
+			snapshot.forEach(function(childSnapshot) { //check each fav president if match change fav button
+		    var childData = childSnapshot.val();
+		    // console.log(childKey);
+		    if(childData.presidentname == currentPresName){
+		    	//show unfav button ontop
+		    	document.getElementById('favBtn').style.zIndex = 0;
+		    }
+	    // ...
+	  	});
+	});
+}
 
 
 
